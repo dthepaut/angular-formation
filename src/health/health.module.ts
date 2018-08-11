2e8f8004dc9766ede3c0a6d57e618ecac56824f3
David <thepaut.david@gmail.com>
2018-08-11 15:03:54 +0200

    crud complete

--- src/health/health.module.ts
+++ src/health/health.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+// Shared modules
+import { SharedModule } from './shared/shared.module'
+
+
+// Guards
 import { AuthGuard } from '../auth/shared/guards/auth.guard'
 
 export const ROUTES: Routes = [
@@ -10,6 +15,6 @@ export const ROUTES: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(ROUTES)]
+  imports: [RouterModule.forChild(ROUTES), SharedModule.forRoot()]
 })
 export class HealthModule {}
--- src/health/shared/shared.module.ts
+++ src/health/shared/shared.module.ts
@@ -3,19 +3,28 @@ import { CommonModule } from '@angular/common'
 import { RouterModule } from '@angular/router'
 
 // third-party modules
-import { AngularFireDatabase } from 'angularfire2/database'
+import { AngularFireDatabaseModule } from 'angularfire2/database'
+
+// components
+import { ListItemComponent } from './components/list-item/list-item.component';
+
+// services
+import { MealsService } from './services/meals/meals.service'
 
 @NgModule({
-  imports: [CommonModule, RouterModule, AngularFireDatabase],
-  declarations: []
+  imports: [
+    CommonModule, 
+    RouterModule, 
+    AngularFireDatabaseModule
+  ],
+  declarations: [ListItemComponent],
+  exports: [ListItemComponent]
 })
 export class SharedModule {
   static forRoot(): ModuleWithProviders {
     return {
       ngModule: SharedModule,
-      providers: [
-
-      ]
+      providers: [MealsService]
     }
   }
 }
